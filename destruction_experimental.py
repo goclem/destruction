6887199499d3c3cbaaa3b626f8bca4dc9686cc6a
Clément Gorin <gorinclem@gmail.com>
2022-05-10 17:22:47 +0200

    Update 2022.05.10

--- destruction_experimental.py
+++ destruction_experimental.py
@@ -4,7 +4,7 @@
 @description: Experimental functions
 @author: Clement Gorin
 @contact: gorinclem@gmail.com
-@version: 2022.05.09
+@version: 2022.05.10
 '''
 
 #%% SEQUENCE GENERATOR
@@ -12,12 +12,12 @@
 # Modules
 import numpy as np
 import itertools
-from destruction_preprocess import get_dates
+from destruction_preprocess import *
 
 # Generates sequences of an arbitatry length
 def generate_sequences(sequence:bool, size:int=2) -> list:
-    index0  = np.where(sequence==0)[0]
-    index1  = np.where(sequence==1)[0]
+    index0 = np.where(sequence==0)[0]
+    index1 = np.where(sequence==1)[0]
     assert len(index0) >= size, 'Sequence smaller than size'
     assert len(index1) >= size, 'Sequence smaller than size'
     sequence0 = itertools.combinations(index0, size)
@@ -33,55 +33,44 @@ sequence = np.tile(sequence, 9).reshape(3, 3, len(sequence))
 sequence = sequence.reshape(3 * 3, 6)
 sequence = np.apply_along_axis(generate_sequences, 1, sequence)
 
-# %% READS SLICES OF RASTER
+#%% FORMATS DATA AS SEQUENCES
 
+import numpy as np
 from destruction_utilities import *
 
+# Reads images as sequences
 images = search_data(pattern('aleppo', 'image'))
 dates  = get_dates(images)
 window = center_window(images[0], (20*128, 20*128))
 images = np.array([read_raster(image, window=window) for image in images])
 images = images_to_sequences(images, tile_size=(128, 128))
+del window
 
+# Reads labels as sequences
 labels = search_data(pattern('aleppo', 'label'))
 window = center_window(labels[0], (20, 20))
 labels = np.array([read_raster(label, window=window, dtype='int8') for label in labels])
 labels = images_to_sequences(labels, tile_size=(1, 1))
 labels = np.squeeze(labels)
+del window
 
+# Keeps sequences with destruction
 analysis  = np.all(labels !=-1, axis=1)
 destroyed = np.any(labels == 3, axis=1)
 images    = images[analysis & destroyed]
 labels    = labels[analysis & destroyed]
 labels    = np.equal(labels, 3)
+del analysis, destroyed
 
+# Checks sequence
 def check_destruction(i):
-    change = int(np.where(np.diff(labels[i]))[0])
-    index  = (change - 2, change + 4)
-    label  = labels[i][index[0]:index[1]]
-    image  = images[i][index[0]:index[1]]
-    date   = dates[index[0]:index[1]]
-    compare(image, date)
+    change  = int(np.where(np.diff(labels[i]))[0]) + 1
+    index   = (change - 2, change + 2)
+    label   = labels[i][index[0]:index[1]]
+    image   = images[i][index[0]:index[1]]
+    caption = dates[index[0]:index[1]]
+    caption = [f'{d}: {l}' for d, l in zip(date, label)]
+    compare(image, caption)
 
-check_destruction(16)
-
-
-index = np.where(labels == 1)[0]
-for i in np.random.choice(index, 5): 
-    display(images[i], f'Label: {labels[i]}')
-
-
-
-date_image = get_dates(images)
-date_label = get_dates(labels)
-list(set(date_label) - set(date_image))
-
-
-#%% DEPRECIATED
-
-source  = '../data/aleppo/images/image_2017-08-14.tif'damages = search_data('damage.gpkg$')
-profile = search_data(pattern('aleppo', 'image'))[0]
-damages = rasterise(damages, profile, '2016-09-18')[11264:13824, 17408:19968]
-damages = np.expand_dims(np.equal(damages, 3), (0, 3))
-damages = np.squeeze(images_to_tiles(damages, tile_size=(128, 128)))
-damages = np.array([measure.block_reduce(damage, (4,4), np.max) for damage in damages])
+check_destruction(2)
+len(generate_sequences(labels[1], 2))
