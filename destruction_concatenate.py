2a8fa6f75001a24d62c49235b5c6ae96956c824d
Arogya Koirala <arogya@berkeley.edu>
2022-07-09 17:43:05 +0545

    updated destruction_concatenate.py 09.07.2022

--- destruction_concatenate.py
+++ destruction_concatenate.py
@@ -1,25 +1,29 @@
+#%%
 from destruction_utilities import *
 from pathlib import Path
 
+#%%
 CITIES = ['aleppo', 'damascus', 'daraa', 'deir-ez-zor', 'hama', 'homs', 'idlib', 'raqqa']
 
+#%%
 def concat(cities, suffix, shape, path = '../data'):
     dest = f"{path}/all/others"
     path = Path(dest)
     path.mkdir(parents=True, exist_ok=True)
     delete_zarr_if_exists('all', suffix)
     zarr.save(f"{dest}/all_{suffix}.zarr", np.empty((0,*shape)))
+    f = zarr.open(f"{dest}/all_{suffix}.zarr", mode='a')
     for city in CITIES:
-        print(f'------ Starting concatenation operation for {city} / suffix: {suffix}')
+        print(f'------ Starting concatenation operation for {city}_{suffix}')
         path = f'../data/{city}/others/{city}_{suffix}.zarr'
         z = zarr.open(path)
-        f = zarr.open(f"{dest}/all_{suffix}.zarr", mode='a')
         idx_array= make_tuple_pair(z.shape[0], 7500)
         for idx_range in idx_array:
             print(f"--------- {idx_range}")
             k = z[0:1000][:]
             f.append(k)
 
+#%%
 concat(CITIES, 'images_conv_train_balanced', (128,128,3))
 concat(CITIES, 'images_conv_valid', (128,128,3))
 concat(CITIES, 'images_conv_test', (128,128,3))
