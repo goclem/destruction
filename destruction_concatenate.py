a4137610856c1ea0f5e846670e31800e630be612
Arogya Koirala <arogya@berkeley.edu>
2022-07-24 06:29:04 +0545

    updates concat script

--- destruction_concatenate.py
+++ destruction_concatenate.py
@@ -25,14 +25,16 @@ def concat(cities, suffix, shape, path = '../data'):
             print(f"--------- {idx_range}")
             k = z[idx_range[0]:idx_range[1]][:]
             f.append(k)
+        print("Done with {city}_{suffix}, deleting now..")
+        delete_zarr_if_exists(city, suffix)
 
 #%%
-concat(CITIES, 'images_conv_train_balanced', (128,128,3))
-concat(CITIES, 'images_conv_valid', (128,128,3))
-concat(CITIES, 'images_conv_test', (128,128,3))
-concat(CITIES, 'labels_conv_train_balanced', (1,1,1))
-concat(CITIES, 'labels_conv_valid', (1,1,1))
-concat(CITIES, 'labels_conv_test', (1,1,1))
+# concat(CITIES, 'images_conv_train_balanced', (128,128,3))
+# concat(CITIES, 'images_conv_valid', (128,128,3))
+# concat(CITIES, 'images_conv_test', (128,128,3))
+# concat(CITIES, 'labels_conv_train_balanced', (1,1,1))
+# concat(CITIES, 'labels_conv_valid', (1,1,1))
+# concat(CITIES, 'labels_conv_test', (1,1,1))
 
 concat(CITIES, 'images_siamese_train_t0_balanced', (128,128,3))
 concat(CITIES, 'images_siamese_train_tt_balanced', (128,128,3))
