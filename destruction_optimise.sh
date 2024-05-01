9e1859e356e011edae96dae938edfe09b4e85c87
dominik.wielath <dominik.wielath@gmail.com>
2024-05-01 18:41:56 +0200

    Files to train the model usig the shell with condor

---
+++ destruction_optimise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Script: destruction_cities_optimise.sh
+# Description: The script calles the python script to train and evaluate the model.
+# Author: Dominik Wielath
+# Date: 2024-05-01
+# Version: 1.0
+
+python destruction_optimise.py --cities moschun aleppo volnovakha
