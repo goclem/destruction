cc9a31a4a22add2ec518a997eaaa2319c1564e63
dominik.wielath <dominik.wielath@gmail.com>
2025-03-14 07:50:28 +0100

    Narrowing down which cities dont work

--- vitmae_finetune_siamese.sh
+++ vitmae_finetune_siamese.sh
@@ -9,14 +9,16 @@
 #python3 destruction_finetune_siamese.py --cities moschun volnovakha
 
 # There seems to be an error when including deirezzor or homs, this has to be investigated
-python3 destruction_finetune_siamese.py --cities hostomel irpin livoberezhnyi moschun volnovakha aleppo daraa hama idlib raqqa
+
+# Looks like Homs and Aleppo didn't work
+#python3 destruction_finetune_siamese.py --cities hostomel irpin livoberezhnyi moschun volnovakha aleppo daraa hama idlib raqqa
 
 
 # Define an array of cities
-#cities=(hostomel irpin livoberezhnyi moschun volnovakha aleppo daraa hama idlib raqqa homs)
+cities=(hostomel aleppo homs)
 
 # Loop over the cities array
-#for city in "${cities[@]}"; do
-#    echo "Processing city: $city"
-#    python3 destruction_finetune_siamese.py --cities "$city"
-#done
+for city in "${cities[@]}"; do
+    echo "Processing city: $city"
+    python3 destruction_finetune_siamese.py --cities "$city"
+done
