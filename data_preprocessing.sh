3d7d7859bf8c0cd37dff86d049d1b9acde3a67c1
dominik.wielath <dominik.wielath@gmail.com>
2025-03-30 19:27:11 +0200

    Changing preprocessing including deirezzor

--- data_preprocessing.sh
+++ data_preprocessing.sh
@@ -8,7 +8,7 @@
 
 #declare -a Cities=("hostomel" "irpin" "livoberezhnyi" "moschun" "volnovakha" "aleppo" "damascus" "daraa" "deirezzor" "hama" "homs" "idlib" "raqqa")
 #declare -a Cities=("damascus" "daraa" "deirezzor" "hama" "homs" "idlib" "raqqa")
-declare -a Cities=("kharkiv") # "volnovakha" "moschun")
+declare -a Cities=("deirezzor") # "volnovakha" "moschun")
 
 for city in "${Cities[@]}"; do
     echo "Preprocessing" $city
