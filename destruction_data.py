8789a8e65f885e0cbb24eab63cd064556764fa71
dominik.wielath <dominik.wielath@gmail.com>
2025-07-16 23:42:04 +0200

    include aleppo and set replace to false

--- destruction_data.py
+++ destruction_data.py
@@ -211,7 +211,7 @@ for sample in ['train', 'valid', 'test']:
     untouch = np.where(~destroy)[0]
     indices = np.concatenate((
         np.where(destroy)[0], # Includes all destroyed samples
-        np.random.choice(untouch, params.sequence_ratio * np.sum(destroy), replace=True))) # Was false, set to true by Dominik
+        np.random.choice(untouch, params.sequence_ratio * np.sum(destroy), replace=False))) # Was false, set to true by Dominik
     
     # Writes destination datasets
     dst_images = zarr.open(dst_images, mode='w', shape=(len(indices), *src_images.shape[1:]), dtype=src_images.dtype)
@@ -291,7 +291,7 @@ for sample in ['train', 'valid', 'test']:
     untouch = np.where(~destroy)[0]
     indices = np.concatenate((
         np.where(destroy)[0],
-        np.random.choice(untouch, params.prepost_ratio * np.sum(destroy), replace=True))) # Was false, set to true by Dominik
+        np.random.choice(untouch, params.prepost_ratio * np.sum(destroy), replace=False))) # Was false, set to true by Dominik
     
     # Writes destination datasets
     dst_images = zarr.open(dst_images, mode='w', shape=(len(indices), *src_images.shape[1:]), dtype=src_images.dtype)
